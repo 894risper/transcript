ab8825011070a454d840aebb72511f16ce5b95af
894risper <145770911+894risper@users.noreply.github.com>
2023-09-27 07:12:33 +0300

    Add files via upload

---
+++ src/trial/tr.java
@@ -0,0 +1,17 @@
+package trial;
+
+public class tr {
+
+	public static void main(String[] args) {
+		int num=0;
+		if (num>0) {
+			System.out.println("the number is positive");
+		}
+
+		else
+		{
+			System.out.println("the number is not positive");
+		}
+		System.out.println("this statement is always executed");
+	}
+}
